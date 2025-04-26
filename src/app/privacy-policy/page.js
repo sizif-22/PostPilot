19e0bfb794b2a9f72f83a60ba2ee6bce00c145c2
Sherif <127263385+sizif-22@users.noreply.github.com>
2025-04-26 21:32:02 +0300

    add privacy-policy page

---
+++ src/app/privacy-policy/page.js
@@ -0,0 +1,9 @@
+const PrivacyPolicy = ()=>{
+     return (
+          <div className="h-screen p-10 bg-gradient-to-b from-[#212121] to-black text-white px-[70px] selection:bg-[#111]">
+               <h1 className="text-4xl">Privacy Policy</h1><br/>
+               <p className="text-xl">coming soon</p>
+          </div>
+     );
+}
+export default PrivacyPolicy;
